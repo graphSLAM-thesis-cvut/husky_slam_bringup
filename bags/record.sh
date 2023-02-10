df5868adb931b2d1e1c348902ee007050fa8e416
Vsevolod Hulchuk <vsevolod.hulchuk@gmail.com>
2023-02-10 14:45:13 +0100

    record fisheye image

--- bags/record.sh
+++ bags/record.sh
@@ -4,5 +4,5 @@ rosbag record /imu/data /imu/data_filtered /imu/data_unbiased \
 /tf /tf_static \
 /husky_velocity_controller/odom /imu_and_wheel_odom /odom_cmd_vel \
 /camera_front/camera_info /camera_front/rectify/parameter_descriptions /camera_front/image_raw/compressed \
-/camera/odom/sample \
+/camera/odom/sample /camera/fisheye1/image_raw/compressed \
 -O test_record.bag
