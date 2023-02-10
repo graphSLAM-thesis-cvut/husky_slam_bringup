a8105bc84b087ed0a65c045159d26a7244878491
subt_deploy <subt_deploy>
2023-02-10 09:40:37 +0100

    Add script to record a bagfile and tf tree pdf

---
+++ bags/record.sh
@@ -0,0 +1,8 @@
+rosbag record /imu/data /imu/data_filtered /imu/data_unbiased \
+/os_cloud_node/imu \
+/points \
+/tf /tf_static \
+/husky_velocity_controller/odom /imu_and_wheel_odom /odom_cmd_vel \
+/camera_front/camera_info /camera_front/rectify/parameter_descriptions /camera_front/image_raw/compressed \
+/camera/odom/sample \
+-O test_record.bag
